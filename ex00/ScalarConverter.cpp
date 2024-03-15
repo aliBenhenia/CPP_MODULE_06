8f422b59497f7260f9a99da16cb7a42a0563be17
Ali ben <alibenhenia1@gmail.com>
2024-03-15 18:32:07 +0000

    add

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: abenheni <abenheni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 22:09:08 by abenheni          #+#    #+#             */
-/*   Updated: 2024/03/14 23:05:51 by abenheni         ###   ########.fr       */
+/*   Updated: 2024/03/15 17:50:39 by abenheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,51 @@ ScalarConverter::ScalarConverter()
 // Testing: After implementing the ScalarConverter class, you need to write a test program. This program should take the input literal from the command line, pass it to the convert method of ScalarConverter, and print the results in the specified format.
 void ScalarConverter::convert(const std::string &input)
 {
-    std::cout << "Convert " << input << std::endl;
-   
+    //check if it's a character literal
+    if (input.size() == 3 && input.at(0) == '\'' && input.at(2) == '\'')
+    {
+        char c = input.at(1);
+        std::cout << "char: ";
+        if (std::isprint(c))
+            std::cout << c << std::endl;
+        else
+            std::cout << "Non displayable" << std::endl;
+        std::cout << "int: " << static_cast<int>(c) << std::endl;
+        std::cout << "float: " << static_cast<float>(c) << "f" << std::endl;
+        std::cout << "double: " << static_cast<double>(c) << std::endl;
+    }
+    else
+    {
+        int                 intValue;
+        float               floatValue;
+        double              doubleValue;
+        std::istringstream  issBuffer(input);
+        if (issBuffer >> intValue)
+        {
+            if (std::isprint(static_cast<char>(intValue)))
+                std::cout << "char: " << static_cast<char>(intValue) << std::endl;
+            else
+                std::cout << "char: impossible" << std::endl;
+            std::cout << "int: " << intValue << std::endl;
+            std::cout << "float: " << static_cast<float>(intValue) << ".0f" << std::endl;
+            std::cout << "double: " << static_cast<double>(intValue) << ".0" << std::endl;
+        }
+        else if (issBuffer.clear(), issBuffer.seekg(0), issBuffer >> floatValue)
+        {
+            std::cout << "char: impossible" << std::endl;
+            std::cout << "int: impossible" << std::endl;
+            std::cout << "float: " << floatValue << "f" << std::endl;
+            std::cout << "double: " << static_cast<double>(floatValue) << std::endl;
+        }
+        else if (issBuffer.clear(), issBuffer.seekg(0), issBuffer >> doubleValue)
+        {
+            std::cout << "char: impossible" << std::endl;
+            std::cout << "int: impossible" << std::endl;
+            std::cout << "float: impossible" << std::endl;
+            std::cout << "double: " << doubleValue << std::endl;
+        }
+        else
+            std::cout << "Invalid literal" << std::endl;
+    }
+    
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,12 +6,13 @@
 /*   By: abenheni <abenheni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 22:09:10 by abenheni          #+#    #+#             */
-/*   Updated: 2024/03/14 22:51:53 by abenheni         ###   ########.fr       */
+/*   Updated: 2024/03/15 16:32:35 by abenheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 class ScalarConverter{
     private:
