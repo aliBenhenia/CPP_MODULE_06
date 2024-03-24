b141e261214acb19f860e2f7a1f5ce03fb21ce6c
Ali ben <alibenhenia1@gmail.com>
2024-03-24 00:49:37 +0000

    update

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,12 +6,12 @@
 /*   By: abenheni <abenheni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 01:20:14 by abenheni          #+#    #+#             */
-/*   Updated: 2024/03/23 18:34:38 by abenheni         ###   ########.fr       */
+/*   Updated: 2024/03/24 00:44:49 by abenheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-
+#include <iomanip> // for std::setprecision and std::fixed
 int checkNumber(const std::string &input)
 {
     if (input.length() == 1 && !isdigit(input[0]))
@@ -39,15 +39,22 @@ int checkChar(const std::string &input)
 
 int checkFloat(const std::string &input)
 {
-    int dotFlag = 0;
     if (input.length() == 1 && !isdigit(input[0]))
         return (0);
+    if (input == "inff" || input == "+inff" || input == "-inff" || input == "nanf")
+        return (FLOAT_);
+    int dotFlag = 0;
+    std::string inputFloat = input;
+    if (input[0] == '+' || input[0] == '-')
+        inputFloat = input.substr(1);
+    if (input.back() == 'f')
+        inputFloat = input.substr(0, input.length() - 1);
     size_t i = 0;
-    while (i < input.length())
+    while (i < inputFloat.length())
     {
-        if (input[i] == '.')
+        if (inputFloat[i] == '.')
             dotFlag++;
-        if (!isdigit(input[i]) && input[i] != '.')
+        if (!isdigit(inputFloat[i]) && inputFloat[i] != '.')
             return (0);
         i++;
     }
@@ -61,6 +68,8 @@ int checkDouble(const std::string &input)
     int dotFlag = 0;
     if (input.length() == 1 && !isdigit(input[0]))
         return (0);
+    if (input == "inf" || input == "+inf" || input == "-inf" || input == "nan")
+        return (DOUBLE_);
     size_t i = 0;
     while (i < input.length())
     {
@@ -113,15 +122,63 @@ void handleChar(const std::string &input)
     std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
 }
 
+int containOnlyZeros(std::string input)
+{
+    int i;
+
+    i = 0;
+    std::string precesion = input.substr(input.find('.') + 1);
+    while (precesion[i])
+    {
+        if (precesion[i] != '0')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+void printFLoat(std::string inputFloat)
+{
+    int f;
+    std::stringstream ss(inputFloat);
+    ss >> f;
+    std::cout << "char: ";
+    if (f < 32 || f > 126)
+        std::cout << "Non displayable" << std::endl;
+    else
+        std::cout << "'" << static_cast<char>(f) << "'" << std::endl;
+    std::cout << "int: " << static_cast<int>(f) << std::endl;
+    std::cout << "float: " << f << ".0f" << std::endl;
+    std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
+}
+
 void handleFloat(const std::string &input)
 {
     std::string inputFloat = input;
     if (input.back() == 'f')
-    {
-        // remove the f at the end
         inputFloat = input.substr(0, input.length() - 1);
+    if (inputFloat == "inff" || inputFloat == "+inff")
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: " << inputFloat << "f" << std::endl;
+        std::cout << "double: " << inputFloat << std::endl;
+        return;
+    }
+    if (input == "nanf")
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+        return;
     }
     float f;
+    if (containOnlyZeros(inputFloat))
+    {
+        printFLoat(inputFloat);
+        return;
+    }
     std::stringstream ss(inputFloat);
     ss >> f;
     std::cout << "char: ";
@@ -136,7 +193,36 @@ void handleFloat(const std::string &input)
 
 void handleDouble(const std::string &input)
 {
-    
+    std::string inputDouble = input;
+    if (input.back() == 'f')
+        inputDouble = input.substr(0, input.length() - 1);
+    if (inputDouble == "inff" || inputDouble == "+inff")
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: " << inputDouble << "f" << std::endl;
+        std::cout << "double: " << inputDouble << std::endl;
+        return;
+    }
+    if (inputDouble == "nan")
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+        return;
+    }
+    double d;
+    std::stringstream ss(inputDouble);
+    ss >> d;
+    std::cout << "char: ";
+    if (d < 32 || d > 126)
+        std::cout << "Non displayable" << std::endl;
+    else
+        std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
+    std::cout << "int: " << static_cast<int>(d) << std::endl;
+    std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+    std::cout << "double: " << d << std::endl;
 }
 
 
